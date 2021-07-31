f498a60228c62f3abb5aed6b40f73c4944e3c930
tbnXC <bwc1024@hotmail.com>
2021-07-31 12:34:48 -0400

    problem 35, while loop and recursive solution

--- 35.py
+++ 35.py
@@ -17,10 +17,61 @@ class Solution:
             
             else: return mid
         
-        return mid
+        return low
 
+class Solution0:
 
-s = Solution()
-nums = [0, 2, 3, 4, 5, 6, 7]
-tar = 3
+    def binSearch(self, val, nums, left, right):
+        if left>right: 
+            return left
+
+        mid = (left+right) // 2
+
+        if val==nums[mid]:
+            return mid
+        
+        elif val > nums[mid]:
+            return self.binSearch(val, nums, mid+1, right)
+
+        else:
+            return self.binSearch(val, nums, left, mid-1)
+
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target<nums[0]: 
+            return 0
+        elif target>nums[-1]: 
+            return len(nums)
+        return self.binSearch(target, nums, 0, len(nums)-1)
+
+class Solution1: #this is a slow solution, the guy in the comments was capping
+
+    def binSearch(self, val, nums, left, right):
+        if left<=right:
+
+            mid = (left+right) // 2
+
+            if val==nums[mid]:
+                return mid
+
+            elif val > nums[mid]:
+                if val<nums[mid+1]:
+                    return mid+1
+
+                else:
+                    return self.binSearch(val, nums, mid+1, right)
+
+            else:
+                return self.binSearch(val, nums, left, mid-1)
+
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        if target<nums[0]: 
+            return 0
+        elif target>nums[-1]: 
+            return len(nums)
+        return self.binSearch(target, nums, 0, len(nums)-1)
+    
+
+s = Solution0()
+nums = [1, 3, 5]
+tar = 4
 print (s.searchInsert(nums, tar))
