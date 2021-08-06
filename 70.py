7e0f632e080199b271fed1cc5358e31373ce5e14
tbnXC <bwc1024@hotmail.com>
2021-08-05 23:09:56 -0400

    memoization and DP for 70

--- 70.py
+++ 70.py
@@ -7,30 +7,32 @@ class Solution:
 
 class Solution0:
     def climbStairs(self, n: int) -> int:
-        return self.recClimb(n, {})
+        return self.recClimb(n, [1, 1])[0]
 
-    def recClimb(self, n: int, d: dict) -> tuple:
-        if n==0 or n==1: 
-            d[n] = 1
-            return [1, d]
-        else:
-            if (n-1) in d:
-                if (n-2) in d:
-                    d[n] = d[n-1] + d[n-2]
-                else:
-                    climb = self.recClimb(n-2, d)
-                    d = climb[1]
-                    d[n] = d[n-1] + climb[0]
-                return [d[n], d]
-            else:
-                if (n-2) in d:
-                    climb = self.recClimb(n-1, d)
-                    d = climb[1]
-                    d[n] = climb[0] + d[n-2]
-                else:
-                    d[n] = self.recClimb(n-1, d) + self.recClimb(n-2, d)
-                return 
+    def recClimb(self, n: int, d: List) -> tuple:
+        if n==1: return [1, d]
+        elif n-2>=len(d):
+            d.append(self.recClimb(n-2, d)[0])
+            d.append(self.recClimb(n-1, d)[0])
+        elif n-1>=len(d):
+            d.append(self.recClimb(n-1, d)[0])
+        return (d[n-1] + d[n-2], d)
+
+class Solution1: 
+    def climbStairs(self, n: int) -> int:
+        if n==1: return 1
+        if n==2: return 2
+        tempMinus2 = 1
+        tempMinus1 = 2
+        while (n>3):
+            tempMinus2, tempMinus1 = tempMinus1, tempMinus1+tempMinus2
+            n-=1
+        return tempMinus1+tempMinus2
+
+            
         
+# print(Solution1().climbStairs(4))
+# print(Solution0().climbStairs(4))
 
-for i in range(0, 10):
-    print(str(i) + ": " + str(Solution0().climbStairs(i)))
+for i in range(1, 46):
+    print(str(i) + ": " + str(Solution0().climbStairs(i)) + " " + str(Solution1().climbStairs(i)))
