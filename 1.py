0fe7c31cc811b99a6ea2b7a906099864986dc001
tbnXC <bwc1024@hotmail.com>
2022-03-22 12:11:26 -0400

    thought I did two sum, but guess not lol. Percentiles on this one seem pretty tough, but I got the solution. Note that the pattern is listed as two pointer, even though that's slower than hash map/dictionary

---
+++ 1.py
@@ -0,0 +1,34 @@
+class Solution0:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        differenceDict = {target-nums[0]: 0}
+        for i in range (1, len(nums)):
+            if nums[i] in differenceDict:
+                return [differenceDict[nums[i]], i]
+            else:
+                differenceDict[target-nums[i]] = i
+            
+
+class Solution1:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        arr = []
+        for i, x in enumerate(nums):
+            arr.append([x, i])
+        arr.sort()  # Sort arr in increasing order by their values.
+        
+        left, right = 0, len(arr) - 1
+        while left < right:
+            sum2 = arr[left][0] + arr[right][0]
+            if sum2 == target:
+                return [arr[left][1], arr[right][1]]
+            elif sum2 > target:
+                right -= 1  # Try to decrease sum2
+            else:
+                left += 1  # Try to increase sum2
+
+class Solution2:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        differenceDict = {}
+        for i in range (len(nums)):
+            if nums[i] in differenceDict:
+                return [differenceDict[nums[i]], i]
+            differenceDict[target-nums[i]] = i
