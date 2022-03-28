545b3198654030a2d751da3db6131dc59fbb6d90
tbnXC <bwc1024@hotmail.com>
2022-03-28 15:03:37 -0400

    unfortunate that I couldn't figure this one out, also some runtime curiotities with solution 2 actually being slower than solution 1

---
+++ 141.py
@@ -0,0 +1,38 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution0:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        temp = head
+        nodes = []
+        while temp:
+            if temp in nodes: return True
+            else: nodes.append(temp)
+            temp = temp.next
+        return False
+
+class Solution1:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        if not head: return False
+        slow = head
+        fast = head.next
+        while fast and fast.next:
+            if slow == fast: return True
+            slow = slow.next
+            fast = fast.next.next
+        return False
+
+class Solution2:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        try:
+            slow = head
+            fast = head.next
+            while slow is not fast:
+                slow = slow.next
+                fast = fast.next.next
+            return True
+        except:
+            return False
