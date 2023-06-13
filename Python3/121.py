6a8070cd9315130005d75df0270d2a347084f9bc
Brayden Cheek <bwc1024@hotmail.com>
2023-06-13 12:24:54 -0400

    Review P121, add new preprocessing solution

--- Python3/121.py
+++ Python3/121.py
@@ -1,6 +1,6 @@
 from typing import List
 
-class Solution0:
+class Solution0: # Brute Force, O(n^2)
     def maxProfit(self, prices: List[int]) -> int:
         maxProfit = 0
         for sellIndex in range (1, len(prices)):
@@ -8,7 +8,7 @@ class Solution0:
                 if prices[sellIndex] - prices[buyIndex] > maxProfit: maxProfit = prices[sellIndex] - prices[buyIndex]
         return maxProfit
     
-class Solution1:
+class Solution1: # Recursive, O(n^2) Don't even know what's going on here lol
     def maxProfit(self, prices: List[int]) -> int:
         minPrice = len(prices)-1
         maxPrice = 0
@@ -21,7 +21,7 @@ class Solution1:
         prices.pop(maxPrice)
         return maxProfit(prices)
 
-class Solution2:
+class Solution2: # This solution I think uses Kadane's Algorithm but with indices, O(n)
     def maxProfit(self, prices: List[int]) -> int:
         left = 0 #Buy
         right = 1 #Sell
@@ -35,15 +35,27 @@ class Solution2:
             right += 1
         return max_profit
 
-class Solution2:
+class Solution3: # Smarter implementation of Kadane's Algorithm, O(n)
     def maxProfit(self, prices: List[int]) -> int:
         maxCur = 0
         maxSoFar = 0
         for i in range(1, len(prices)):
             maxCur += prices[i]
-            maxCur = max(0, maxCur - prices[i-1])
+            maxCur = max(0, maxCur - prices[i-1]) ## Sell value - buy value
             maxSoFar = max(maxCur, maxSoFar)
         return maxSoFar
 
+class Solution4: # Same as Solution3 but with a list of differences, O(n)
+    def maxProfit(self, prices: List[int]) -> int:
+        diffList = [0] + [prices[i]-prices[i-1] for i in range(1, len(prices))]
+        
+        maxProfit = profit = 0
+
+        for buyIndex in range(len(prices)):
+            profit = max(profit + diffList[buyIndex], 0)
+            if profit>maxProfit: maxProfit = profit
+
+        return maxProfit
+        
 s = Solution2()
 prices = []
